da3a0ff5109449607c06adc40889e74655cfc439
SHadeleaf <ellguan.ms@gmail.com>
2025-07-23 16:09:02 -0500

    breadcrumbs of a scrolling card deck

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+var websitesTitle = document.getElementById('websitesTitle');
+//var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+var height = websitesTitle.offsetHeight;
+//console.log(background.offsetHeight*100/h);
+
+
+
 /* When clicked, scrolls window to element */
 function scrollWindow(element) {
  const scroll = document.getElementById(element);
  scroll.scrollIntoView({behavior: "smooth", block: "center"});
 }
+
